3967f5c84f3b70eaeb155d8897dee83726b488f6
Sandro <sandro33810@gmail.com>
2026-06-27 17:00:39 +0200

    third commit

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -50,17 +50,148 @@ fn main() {
 
     let s1 = String::from("Hello");
 
-    let (s2, len) = calculate_length(s1);
+    //let (s2, len) = calculate_length(s1);
 
-    println!("The length of '{s2}' is {len}");
+    //println!("The length of '{s2}' is {len}");
 
-}
 
-fn calculate_length(s: String) -> (String, usize) {
+    //
+
+    let s1 = String::from("Hello");
+
+    //let len = calculate_length(&s1);
+
+    //println!("The length of {s1} is {len}");
+
+    //change(&s);
+
+
+    //
+
+    let mut s = String::from("Hello");
+
+    change(&mut s);
+    {
+        let r1 = &mut s;
+
+        println!("{r1}");
+    }
+    let r2 = &mut s;
+
+    println!("{r2}");
+
+    let mut s = String::from("Hello");
+    let r1 = &s;
+    let r2 = &s;
+    println!("{r1} and {r2}");
+
+    let r3 = &mut s;
+    println!("{r3}");
+
+
+    //
+
+    let reference_to_nothing = dangle();
+
+
+    //
+
+
+    let mut s = String::from("hello world");
+    let word = first_word(&s);
+
+    s.clear();
+
+
+    //
+
+    let s = String::from("hello world");
+
     let length = s.len();
 
-    (s, length)
+    let hello = &s[..5];
+    let world = &s[6..]; // Ou
+    let world = &s[6..length];
+
+    //let (first_word, index) = first_word_updated(&s);
+
+    //s.clear();
+
+    //println!("The first word of '{s}' is '{first_word}' and this is the index '{index}'");
+
+    let my_string = String::from("Hello world");
+
+    let word = first_word_updated(&my_string[0..6]);
+    let word = first_word_updated(&my_string[..]);
+
+    let word = first_word_updated(&my_string);
+
+    let my_string_literal = "Hello world";
+
+    let word = first_word_updated(&my_string_literal[0..6]);
+    let word = first_word_updated(&my_string_literal[..]);
+
+    let word = first_word_updated(&my_string_literal);
+
+
+    //
+
+    let a = [1, 2, 3, 4, 5];
+
+    let slice = &a[1..3];
+
+    assert_eq!(slice, &[2, 3]);
 }
+
+fn first_word_updated(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' '  {
+            return &s[..i];
+        }
+    }
+
+    &s[..]
+}
+
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
+}
+
+fn dangle() -> String {
+    let s = String::from("Hello");
+
+    s
+}
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
+
+
+
+
+// fn change(some_string: &String) {
+//     some_string.push_str(", world");
+// }
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+// fn calculate_length(s: String) -> (String, usize) {
+//     let length = s.len();
+//
+//     (s, length)
+// }
 
 fn gives_ownership() -> String {
     let some_string = String::from("yours");
