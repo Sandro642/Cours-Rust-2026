36084e76670671443e0d2b2a289d5eb90d37b2e2
sandraw <sandro.soria@proton.me>
2026-06-29 20:03:10 +0200

    New commit

--- Struct/src/main.rs
+++ Struct/src/main.rs
@@ -1,65 +1,76 @@
-// struct User {
-//     active: bool,
-//     username: String,
-//     email: String,
-//     sign_in_count: u64,
-// }
-
-// struct User {
-//     active: bool,
-//     username: &str,
-//     email: &str,
-//     sign_in_count: u64,
-// }
-
-struct Color(i32, i32, i32);
-struct Point(i32, i32, i32);
-
-struct AlwaysEqual;
-
-fn main() {
-    // let mut user1 = User {
-    //     active: true,
-    //     username: String::from("someusername123"),
-    //     email: String::from("someone@example.com"),
-    //     sign_in_count: 1,
-    // };
-    //
-    // user1.email = String::from("anotheremail@exemple.com");
-    //
-    // // let user2 = User {
-    // //     active: user1.active,
-    // //     username: user1.username,
-    // //     email: String::from("another@example.com"),
-    // //     sign_in_count: user1.sign_in_count,
-    // // };
-    //
-    // // |
-    //
-    // let user2 = User {
-    //     email: String::from("another@example.com"),
-    //     ..user1
-    // };
-    //
-    // let black = Color(0, 0, 0);
-    // let origin = Point(0, 0, 0);
-    //
-    // let subject = AlwaysEqual;
-
-    // let user1 = User {
-    //     active: true,
-    //     username: "someusername123",
-    //     email: "someone@example.com",
-    //     sign_in_count: 1,
-    // };
-
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
 
-// fn build_user(email: String, username: String) -> User {
-//     User {
-//         active: true,
-//         username,
-//         email,
-//         sign_in_count: 1,
-//     }
-// }
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+}
+
+// Inutile de séparer ces méthodes en plusieurs blocs Impl mais cette syntaxe fonctionne.
+impl Rectangle {
+    fn can_hold(&self, rectangle: &Rectangle) -> bool {
+        self.width >= rectangle.width && self.height >= rectangle.height
+    }
+
+    fn square(size: u32) -> Self  {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    if rect1.width() {
+        println!(
+            "The rectangle has a nonzero width; it is {}",
+            rect1.width
+        );
+    }
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+
+
+    //
+
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect1 hold rect2 ? {}", rect1.can_hold(&rect2));
+    println!("Can rect 1 hold rect3 ? {}", rect1.can_hold(&rect3));
+
+
+    //
+
+    let sq = Rectangle::square(3);
+
+
+    //
+
+}
