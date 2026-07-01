c306c901f4f7e860b40b70e5ea59df16a840be2c
sandraw <sandro.soria@proton.me>
2026-07-01 22:18:01 +0200

    Update project configuration and add match and if-let examples in Rust

--- matchs/src/main.rs
+++ matchs/src/main.rs
@@ -52,6 +52,57 @@ fn main() {
         7 => remove_fancy_hat(),
         _ => (),
     }
+
+    let config_max = Some(3u8);
+
+    match config_max {
+        Some(max) => println!("The maximum is condigured to be {max}"),
+        _ => (),
+    }
+
+    // let mut count = 0;
+    // match coin {
+    //     Coin::Quarter(state) => println!("State quarter from {state:?}"),
+    //     _ => count += 1,
+    // }
+    //
+    // let mut count = 0;
+    //
+    // if let Coin::Quarter(state) = coin {
+    //     println!("State gaurter from {state:?}");
+    // } else {
+    //     count += 1;
+    // }
+
+    if let Some(max) = config_max {
+        println!("The maximum is configured to be {max}");
+    }
+
+    // let mut count = 0;
+    // match coin {
+    //     Coin::Quarter(state) => println!("State quarter from {state:?}"),
+    //     _ => count += 1,
+    // }
+    //
+    // let mut count = 0;
+    //
+    // if let Coin::Quarter(state) = coin {
+    //     println!("State gaurter from {state:?}");
+    // } else {
+    //     count += 1;
+    // }
+
+
+
+}
+
+impl UsState {
+    fn existed_in(&self, year: u16) -> bool {
+        match self {
+            UsState::Alabama => year >= 1819,
+            UsState::Alaska => year >= 1959,
+        }
+    }
 }
 
 fn add_fancy_hat() {}
