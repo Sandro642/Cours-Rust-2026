c886ff2544f856442a45bf1114a444cd0a90157d
sandraw <sandro.soria@proton.me>
2026-07-06 23:57:24 +0200

    Add `rand` dependency and expand `restaurant` project with new modules and examples

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,11 +1,69 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
+// mod front_of_house {
+//     pub mod hosting {
+//         pub fn add_to_waitlist() {}
+//     }
+// }
+//
+// pub fn eat_at_restaurant() {
+//     crate::front_of_house::hosting::add_to_waitlist();
+//
+//     front_of_house::hosting::add_to_waitlist();
+// }
+//
+//
+// fn deliver_order() {}
+//
+// mod back_of_house {
+//     fn fix_incorrect_order() {
+//         cook_order();
+//         super::deliver_order();
+//     }
+//
+//     fn cook_order() {}
+// }
+
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+
+    }
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
     }
 }
 
-pub fn eat_at_restaurant() {
-    crate::front_of_house::hosting::add_to_waitlist();
+use crate::back_of_house::Breakfast;
+use crate::back_of_house::Appetizer::Salad;
 
-    front_of_house::hosting::add_to_waitlist();
+mod customer {
+    pub fn eat_at_restaurant() {
+        let mut meal = super::Breakfast::summer("Rye");
+
+        meal.toast = String::from("Wheat");
+
+        println!("I'd like {} toast please", meal.toast);
+
+        let order1 = super::Salad;
+        let order2 = super::back_of_house::Appetizer::Soup;
+    }
+}
+
+mod front_of_house;
+
+pub use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
 }
