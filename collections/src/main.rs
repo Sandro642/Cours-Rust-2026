842f781a60496f669e6d81b266c0e8ddc0b6f0e4
sandraw <sandro.soria@proton.me>
2026-07-07 16:43:57 +0200

    Set up `collections` project with examples for vectors and updated project configuration

---
+++ collections/src/main.rs
@@ -0,0 +1,25 @@
+fn main() {
+    let mut v: Vec<i32> = Vec::new();
+
+    v.push(5);
+    v.push(6);
+    v.push(7);
+    v.push(8);
+
+    let v = vec![1,2,3];
+
+    let v = vec![1,2,3,4,5];
+
+    let third: &i32 = &v[2];
+    println!("The third element is {third}");
+
+    let third: Option<&i32> = v.get(2);
+    match third {
+        Some(third) => println!("The third element is {third}"),
+        None => println!("There is no third element"),
+    }
+
+    let does_not_exist = &v[100];
+    let does_not_exist = v.get(100);
+
+}
