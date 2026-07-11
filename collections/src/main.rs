54e8b44274f5962153b4bf5899e37a5298abe628
sandraw <sandro.soria@proton.me>
2026-07-11 12:45:49 +0200

    Expand `collections` project with examples for strings, including creation, mutation, and formatting

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -19,8 +19,8 @@ fn main() {
         None => println!("There is no third element"),
     }
 
-    let does_not_exist = &v[100];
-    let does_not_exist = v.get(100);
+    // let does_not_exist = &v[100];
+    // let does_not_exist = v.get(100);
 
     //
 
@@ -57,4 +57,68 @@ fn main() {
         }
     }
 
+    //
+
+    let mut s = String::new();
+
+    let data = "initial contents";
+
+    let s = data.to_string();
+
+    // The same
+
+    let s = "Initial contents".to_string();
+
+    let s = String::from("initial contents");
+
+    let hello = String::from("السلام عليكم");
+    let hello = String::from("Dobrý den");
+    let hello = String::from("Hello");
+    let hello = String::from("שלום");
+    let hello = String::from("नमस्ते");
+    let hello = String::from("こんにちは");
+    let hello = String::from("안녕하세요");
+    let hello = String::from("你好");
+    let hello = String::from("Olá");
+    let hello = String::from("Здравствуйте");
+    let hello = String::from("Hola");
+
+    let mut s = String::from("foo");
+
+    s.push_str("bar");
+
+    let mut s1 = String::from("foo");
+    let s2 = "bar";
+    s1.push_str(s2);
+    println!("s2 is {s2}");
+
+    let mut s = String::from("lo");
+    s.push('l'); // Only a char
+
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    // let s3 = s1 + &s2;
+    let s2 = String::from("Tout le monde!");
+    let s4 = format!("{s1} {s2}");
+    println!("{s2}");
+
+    //
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = format!("{s1}-{s2}-{s3}");
+
+    // let s1 = String::from("hi");
+    // let h = s1[0];
+
+    let hello = String::from("Hola");
+    let s: &str = &hello[..2];
+    println!("{s}");
+
+
+    for c in hello.bytes() {
+        println!("{c}");
+    }
 }
