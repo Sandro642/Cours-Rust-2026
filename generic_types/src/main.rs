3588c7e112d530d210902d959f48eadcc648e778
sandraw <sandro.soria@proton.me>
2026-07-21 13:09:49 +0200

    Expand `generic_types` project with examples for struct implementations and method mixing

--- generic_types/src/main.rs
+++ generic_types/src/main.rs
@@ -1,4 +1,5 @@
-use crate::higher_algo::use_mod;
+use crate::complex_type::use_ugly_impl;
+use crate::type_parameter::use_impl;
 
 fn main() {
     let number_list = vec![34,50,25,100,65];
@@ -26,31 +27,145 @@ fn main() {
 
     println!("Le nombre le plus grand est {}", le_plus_grand);
 
-    use_mod();
+    // use_mod();
+
+    use_impl();
+    use_ugly_impl();
 }
 
-pub mod higher_algo {
-    fn le_plus_grand(list: &[i32]) -> i32 {
-        let mut the_higher = list[0];
+// pub mod higher_algo {
+//     // fn le_plus_grand(list: &[i32]) -> i32 {
+//     //     let mut the_higher = list[0];
+//     //
+//     //     for &element in list {
+//     //         if element > the_higher {
+//     //             the_higher = element;
+//     //         }
+//     //     }
+//     //
+//     //     the_higher
+//     // }
+//
+//     fn le_plus_grand_i32(list: &[i32]) -> i32 {
+//         let mut le_plus_grand = list[0];
+//
+//         for &element in list.iter() {
+//             if element > le_plus_grand {
+//                 le_plus_grand = element;
+//             }
+//         }
+//
+//         le_plus_grand
+//     }
+//
+//     fn le_plus_grand_caractere(list: &[char]) -> char {
+//         let mut le_plus_grand = list[0];
+//
+//         for &element in list.iter() {
+//             if element > le_plus_grand {
+//                 le_plus_grand = element;
+//             }
+//         }
+//
+//         le_plus_grand
+//     }
+//
+//     fn le_plus_grand<T>(list: &[T]) -> T {
+//         let mut le_plus_grand = list[0];
+//
+//         for &element in list {
+//             if element > le_plus_grand {
+//                 le_plus_grand = element;
+//             }
+//         }
+//
+//         le_plus_grand
+//     }
+//
+//     pub fn use_mod() {
+//         let number_list_1 = vec![34,50,25,100,65];
+//
+//         let result = le_plus_grand(&number_list_1);
+//         println!("Le nombre le plus grand est {}", result);
+//
+//         let number_list_2 = vec![102,34,6000,89,54,2,43,8];
+//
+//         let result = le_plus_grand(&number_list_2);
+//         println!("Le nombre le plus grand est {}", result);
+//
+//         //
+//
+//         let result = le_plus_grand(&number_list_1);
+//         println!("Le plus grand nombre est {}", result);
+//
+//         let char_list_1 = vec!['y', 'm', 'a', 'q'];
+//
+//         let result = le_plus_grand(&char_list_1);
+//         println!("Le plus grand caractère est {}", result);
+//     }
+// }
 
-        for &element in list {
-            if element > the_higher {
-                the_higher = element;
-            }
-        }
-
-        the_higher
+pub mod type_parameter {
+    struct Point<T> {
+        x: T,
+        y: T,
     }
 
-    pub fn use_mod() {
-        let number_list_1 = vec![34,50,25,100,65];
+    impl<T> Point<T> {
+        fn x(&self) -> &T {
+            &self.x
+        }
 
-        let result = le_plus_grand(&number_list_1);
-        println!("Le nombre le plus grand est {}", result);
+        fn y(&self) -> &T {
+            &self.y
+        }
+    }
 
-        let number_list_2 = vec![102,34,6000,89,54,2,43,8];
+    fn use_struct() {
+        let entiers = Point {x: 5, y: 10};
+        let flottants = Point {x: 1.0, y: 4.0};
 
-        let result = le_plus_grand(&number_list_2);
-        println!("Le nombre le plus grand est {}", result);
+        // let ne_fonctionne_pas = Point {x: 1, y: 2.0};
+    }
+
+    pub fn use_impl() {
+        let p = Point {x:5, y:10};
+
+        println!("p.x = {}", p.x());
+    }
+
+    enum Option<T> {
+        Some(T),
+        None,
+    }
+
+    enum Result<T, E> {
+        Ok(T),
+        Err(E),
+    }
+}
+
+pub mod complex_type {
+    struct Point<X1,Y1>  {
+        x: X1,
+        y: Y1,
+    }
+
+    impl<X1,Y1> Point<X1,Y1> {
+        fn melange<X2,Y2>(self, other: Point<X2,Y2>) -> Point<X1,Y2> {
+            Point {
+                x: self.x,
+                y: other.y,
+            }
+        }
+    }
+
+    pub fn use_ugly_impl() {
+        let p1 = Point {x:5, y:10.4};
+        let p2 = Point {x: "Hello", y: 'c'};
+
+        let p3 = p1.melange(p2);
+
+        println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
     }
 }
