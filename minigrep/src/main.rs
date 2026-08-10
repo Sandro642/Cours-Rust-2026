8114ec4f4a3afe3864b244aa23d1480b98d32563
Sandro642 <sandro33810@gmail.com>
2026-08-10 11:18:29 +0200

    New commit updated Minigrep, reading file

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,14 +4,20 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    let (recherche, nom_fichier) = interpreter_config(&args);
+
+    println!("On recherche : {}", recherche);
+    println!("Dans le fichier : {}", nom_fichier);
+
+    let contenu = fs::read_to_string(nom_fichier)
+        .expect("Quelque chose s'est mal passé lors de la lecture du fichier");
+
+    println!("Dans le texte :\n{}", contenu);
+}
+
+fn interpreter_config(args: &[String]) -> (&str, &str) {
     let recherche = &args[1];
-    let nom_du_fichier = &args[2];
+    let nom_fichier = &args[2];
 
-    println!("On recherche {}", recherche);
-    println!("Dans le fichier {}", nom_du_fichier);
-
-    let contenu = fs::read_to_string(nom_du_fichier)
-        .expect("Quel que chose s'est mal passé durant la lecture du fichier.");
-
-    println!("Dans le texte :\n {}", contenu);
+    (recherche, nom_fichier)
 }
