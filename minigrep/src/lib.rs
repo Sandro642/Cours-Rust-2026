5da25d50cfb7c70030281e1877344f7372fae87d
Sandro642 <sandro33810@gmail.com>
2026-09-04 17:31:18 +0200

    Update minigrep crate

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,10 +1,17 @@
+use std::{env, result};
 use std::{error::Error, fs};
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contenu = fs::read_to_string(config.nom_fichier)?;
 
-    for ligne in rechercher(&config.recherche, &contenu) {
-        println!("{}", ligne);
+    let results = if config.ignorer_majuscules {
+        search_case_insensitivity(&config.recherche, &contenu)
+    } else {
+        rechercher(&config.recherche, &contenu)
+    };
+
+    for line in results {
+        println!("{line}");
     }
 
     Ok(())
@@ -13,6 +20,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 pub struct Config {
     pub recherche: String,
     pub nom_fichier: String,
+    pub ignorer_majuscules: bool,
 }
 
 impl Config {
@@ -24,11 +32,27 @@ impl Config {
         let recherche = args[1].clone();
         let nom_fichier = args[2].clone();
 
+        let ignorer_majuscules = env::var("IGNORE_CASE").is_ok();
+
         Ok(Config {
             recherche,
             nom_fichier,
+            ignorer_majuscules,
         })
     }
+}
+
+pub fn search_case_insensitivity<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in content.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
 
 pub fn rechercher<'a>(recherche: &str, contenu: &'a str) -> Vec<&'a str> {
@@ -58,5 +82,20 @@ Obtenez les trois en même temps.";
             vec!["sécurité, rapidité, productivité."],
             rechercher(recherche, contenu)
         );
+    }
+
+    #[test]
+    fn case_insensitivity() {
+        let query = "rUsT";
+        let content = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitivity(query, content)
+        )
     }
 }
